ac1d0185ec262a4f0832d1f660bd496b04ac32de
zimsecpapers <farmplus93@gmail.com>
2023-02-05 20:48:55 +0100

    improved search functionality

--- components/pg/prod_list.tsx
+++ components/pg/prod_list.tsx
@@ -272,7 +272,57 @@ function ProdList({ trendingProducts,cartButtonColors,ip,header }: { trendingPro
 
             {fr && memoizedFilteredProductsList.length===0 &&(
                 <div>
-                    <p className="text-xl font-medium">Sorry We Couldn't find anything related to your search query !!!</p>
+                    <div className="border border-red-200 rounded-md p-2 ">
+                        <p className="text-xl font-medium">Sorry We Couldn't find anything related to your search query !!!</p>
+                    </div>
+                    <h4 className="mt-5 text-xl font-bold">Other Products</h4>
+                    <div className="mt-1 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
+                    {trendingProducts.map((product)=>{
+                        let isTrending:boolean=product.isTrending;
+                        if(true){
+                            return(
+                                <div key={product.productId} className="border rounded-lg p-2 overflow-hidden">
+
+                                    <Link href={`/product/${product.productId}`}>
+                                        <div className="group cursor-pointer flex flex-col items-center justify-center">
+                                                <img
+                                                     src={product.mainImageUrl}
+                                                     alt={product.productName}
+                                                     className="h-60 w-auto object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                                                   />
+                                                <p>{product.productName}</p>
+                                        </div>
+                                    </Link>
+
+                                    <div  className="grid grid-cols-3 mt-2 items-center justify-center">
+
+                                      <p className="text-slate-800">{product.condition}</p>
+
+                                      <div className="flex items-center justify-center space-x-2">
+                                        <p className="text-lg font-bold">{product.price}</p>
+                                        <span className="text-[10px]">{product.currency}</span>
+                                      </div>
+
+
+                                      {/* add to cart button*/}
+                                      <div
+                                        onClick={()=>{addProductToCart(product);}}
+                                        className={`${tempCartBbColor[product.productId]} hover:cursor-pointer flex items-center justify-center rounded-md p-1 text-white`}>
+                                        <svg className="h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                                        </svg>
+                                        <div className="text-sm md:text-base">Add To Cart</div>
+                                      </div>
+                                      {/* End of cart */}
+
+                                    </div>
+
+                                </div>
+                            );
+                        }
+                    })}
+                    </div>
+                    {/*End of Trending Products*/}
                 </div>
             )}
 
